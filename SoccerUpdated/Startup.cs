842faf71d65c791ea31d1b7585fad7db0fc19a1b
prabin89 <prabindrasapkota89@gmail.com>
2020-07-17 16:11:13 +0545

    Add project files.

---
+++ SoccerUpdated/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoccerUpdated.Startup))]
+namespace SoccerUpdated
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
